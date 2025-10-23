0cc40a50839119ebbe6df14c392e3def7d20848e
Ryan-Fx <rinnd.dn@gmail.com>
2025-10-23 21:45:38 +0800

    fix: adjust header layout and text styling for improved readability

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
   const pathname = usePathname();
   return (
     <header className="text-center fixed left-0 top-0 z-20 right-0 rounded-bl-full rounded-tl-full rounded-br-full p-4 m-4 bg-[url('/assets/bgtext.png')] bg-cover bg-center bg-no-repeat">
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between lg:pr-10">
         <div className="flex items-center justify-start space-x-4">
           <Link href="/">
             <Image
@@ -21,14 +21,14 @@ export default function Header() {
               className="rounded-full size-20 animate-spin lg:size-[130px] border-2 lg:border-4 border-pink-500"
             />
           </Link>
-          <h1 className="text-lg lg:text-4xl bg-linear-to-r font-semibold from-purple-500 via-yellow-400 to-yellow-400 bg-clip-text text-transparent">
+          <h1 className="text-md lg:text-4xl bg-linear-to-r font-semibold from-purple-500 via-yellow-400 to-yellow-400 bg-clip-text text-transparent">
             <Typewriter
               words={[
                 "Hi! Bella here!✨",
-                "Learn new vocabularies everyday!",
-                "Make new sentences everyday!",
-                "Learn new phrases!",
-                "Learn new idioms!",
+                "Learn new vocabularies!",
+                "Make new sentences!✨",
+                "Learn new phrases!✨",
+                "Learn new idioms!✨",
                 "Learn new phrasal verbs!",
               ]}
               loop={0} // infinite loop
