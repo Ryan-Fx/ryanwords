e6744f55ed6cbe799a79de5eba6312913ee04517
Ryan-Fx <rinnd.dn@gmail.com>
2025-10-24 15:18:14 +0800

    fix: improve header text styling and add new typewriter word for enhanced user engagement

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
   return (
     <header className="text-center fixed left-0 top-0 z-20 right-0 rounded-bl-full rounded-tl-full rounded-br-full p-4 m-4 bg-[url('/assets/bgtext.png')] bg-cover bg-center bg-no-repeat">
       <div className="flex items-center justify-between lg:pr-10">
-        <div className="flex items-center justify-start space-x-4">
+        <div className="flex items-center justify-start space-x-4 text-sm">
           <Link href="/">
             <Image
               src="/assets/okeejoss.png"
@@ -21,10 +21,11 @@ export default function Header() {
               className="rounded-full size-20 lg:size-[130px] border-2 lg:border-4 border-pink-500"
             />
           </Link>
-          <h1 className="text-md lg:text-4xl bg-linear-to-r font-semibold from-purple-500 via-yellow-400 to-yellow-400 bg-clip-text text-transparent">
+          <h1 className="text-md lg:text-4xl bg-linear-to-r font-semibold from-purple-500 via-yellow-400 to-yellow-400 bg-clip-text text-transparent py-2">
             <Typewriter
               words={[
                 "Hi! Ryan here!✨",
+                "The things to do everyday!",
                 "Learn new vocabularies!",
                 "Make new sentences!✨",
                 "Learn new phrases!✨",
